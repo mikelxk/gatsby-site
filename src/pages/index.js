b2123636508771b5b2150cc24c4854a89a92d48c
Xiaokai Li <mike.lxk@gmail.com>
2021-03-25 21:49:36 +0800

    chore: update syntax

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,23 @@
-import * as React from "react"
+import React from "react"
 import Badges from "../components/Badges"
 import SEO from "../components/seo"
 import avatar from "../images/avatar.webp"
 
 const IndexPage = () => (
-  <div className="mx-auto max-w-screen-md px-4 sm:px-6 md:px-8 my-12 sm:my-20 md:my-32">
+  <div class="mx-auto max-w-screen-md px-4 sm:px-6 md:px-8 my-12 sm:my-20 md:my-32">
     <SEO title="Home" />
-    <div className="flex flex-col sm:flex-row">
+    <div class="flex flex-col sm:flex-row">
       <img
-        className="w-24 md:w-32 md:h-32 rounded-full"
+        class="w-24 h-24 md:w-32 md:h-32 rounded-full"
         src={avatar}
         alt="avatar"
       />
-      <div className="mt-8 sm:mt-0 sm:ml-8 flex flex-col justify-center">
-        <p className="text-2xl md:text-3xl leading-tight">Hi, I'm</p>
-        <h1 className="leading-tight">Xiaokai Li</h1>
+      <div class="mt-8 sm:mt-0 sm:ml-8 flex flex-col justify-center">
+        <p class="text-2xl md:text-3xl leading-tight">Hi, I'm</p>
+        <h1 class="leading-tight">Xiaokai Li</h1>
       </div>
     </div>
-    <p className="mt-10 text-lg leading-7">
+    <p class="mt-10 text-lg leading-7">
       I'm currently an undergraduate at University of Florida. I'm interested in
       C++ and TypeScript.
     </p>
