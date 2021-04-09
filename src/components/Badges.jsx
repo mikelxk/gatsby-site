3254e120fbe7e5381fce512e195f73da37300f82
Xiaokai Li <mike.lxk@gmail.com>
2021-04-10 03:40:15 +0800

    add discernible name

--- src/components/Badges.jsx
+++ src/components/Badges.jsx
@@ -1,11 +1,10 @@
 import React from "react"
 import { FaTwitter, FaBroadcastTower, FaEnvelope } from "react-icons/fa"
 import { GoMarkGithub } from "react-icons/go"
-const Badge = ({ Icon, href }) => (
+const Badge = ({ Icon, href, text }) => (
   <a
     href={href}
-    target="_blank"
-    rel="noopener noreferrer"
+    aria-label={text}
     className="text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white transition duration-150 ease-in-out"
   >
     <Icon size="24" />
@@ -13,10 +12,22 @@ const Badge = ({ Icon, href }) => (
 )
 const Badges = () => (
   <div className="mt-10 flex space-x-16">
-    <Badge Icon={GoMarkGithub} href="https://github.com/mikelxk" />
-    <Badge Icon={FaTwitter} href="https://twitter.com/mike_lxk" />
-    <Badge Icon={FaEnvelope} href="mailto:mike.lxk@gmail.com" />
-    <Badge Icon={FaBroadcastTower} href="https://mikelxk.github.io/" />
+    <Badge
+      Icon={GoMarkGithub}
+      href="https://github.com/mikelxk"
+      text="Github"
+    />
+    <Badge
+      Icon={FaTwitter}
+      href="https://twitter.com/mike_lxk"
+      text="Twitter"
+    />
+    <Badge Icon={FaEnvelope} href="mailto:mike.lxk@gmail.com" text="Email" />
+    <Badge
+      Icon={FaBroadcastTower}
+      href="https://mikelxk.github.io/"
+      text="Blog"
+    />
   </div>
 )
 
